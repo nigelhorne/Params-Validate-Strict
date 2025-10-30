f64e1d6d2a8c9e86aeda66b5017322905913bc3f
Nigel Horne <njh@bandsman.co.uk>
2025-10-30 13:38:15 -0400

    Release 0.23

--- lib/Params/Validate/Strict.pm
+++ lib/Params/Validate/Strict.pm
@@ -18,11 +18,11 @@ Params::Validate::Strict - Validates a set of parameters against a schema
 
 =head1 VERSION
 
-Version 0.22
+Version 0.23
 
 =cut
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 =head1 SYNOPSIS
 
